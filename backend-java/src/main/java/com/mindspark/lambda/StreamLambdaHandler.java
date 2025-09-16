038d651170e56babf32efaf895a09fd340db68fd
Yingjian Ding <james.yingjian.ding@gmail.com>
2025-09-16 00:26:05 -0700

    update cdk

---
+++ backend-java/src/main/java/com/mindspark/lambda/StreamLambdaHandler.java
@@ -0,0 +1,17 @@
+package com.mindspark.lambda;
+
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class StreamLambdaHandler implements RequestStreamHandler {
+
+
+    @Override
+    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
+
+    }
+} 
